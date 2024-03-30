0abf7bbc84d50ea9427ca82713a69a112e4fca89
Rohith Gilla <gillarohith1@gmail.com>
2024-03-30 23:29:54 +0530

    feat: Presentation in the app

--- obol-dapp-challenge/packages/ui/src/molecules/cards/image-card.tsx
+++ obol-dapp-challenge/packages/ui/src/molecules/cards/image-card.tsx
@@ -1,25 +1,22 @@
-import { Button } from "../../atoms/button";
 import React from "react";
 import Typography from "../../atoms/typography";
 
 interface ImageCardProps {
   title: string;
-  description: string;
-  linkText: string;
-  onLinkClick?: () => void;
+  description?: string;
   // Assuming there will be a prop for the image URL or any other image-related data
   imageUrl: string;
+  children?: React.ReactNode;
 }
 
 const ImageCard: React.FC<ImageCardProps> = ({
   title,
   description,
-  linkText,
-  onLinkClick,
   imageUrl,
+  children,
 }) => {
   return (
-    <div className="bg-bg-2 rounded-lg p-6 text-white max-w-md">
+    <div className="bg-bg-2 rounded-lg p-6 text-white max-w-lg">
       <div
         className="bg-gray-500 h-60 rounded-lg"
         style={{
@@ -29,15 +26,17 @@ const ImageCard: React.FC<ImageCardProps> = ({
           backgroundRepeat: "no-repeat",
         }}
       />
-      <Typography className="mt-4 font-semi-bold" variant="h3">
-        {title}
-      </Typography>
-      <Typography className="mt-4 text-[#9CC2C9]" variant="body">
-        {description}
-      </Typography>
-      <Button onClick={onLinkClick} variant="primary">
-        {linkText}
-      </Button>
+      <div className="flex flex-col items-center align-center">
+        <Typography className="mt-4 font-semi-bold" variant="h3">
+          {title}
+        </Typography>
+        {description && (
+          <Typography className="mt-4 text-[#9CC2C9]" variant="body">
+            {description}
+          </Typography>
+        )}
+        {children}
+      </div>
     </div>
   );
 };
